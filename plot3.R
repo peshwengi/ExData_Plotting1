ae4f42b36a3e9168164dcb38186322047aa211f6
James Bywater <james@coffeemonsters.com>
2014-05-11 22:54:01 +0530

    Fixed typo

--- plot3.R
+++ plot3.R
@@ -8,14 +8,14 @@ power = subset( power, Date == as.Date( "2007-02-01" )
 #Create a dateTime column which contains a time and date value
 #as.POSIXct automatically uses strptime() if you pass in character paramaters 
 #so no need to call that explicitly
-power$dateTime <- as.POSIXct( paste( power$Date, power$Time ), format="%F %T" )
+power$datetime <- as.POSIXct( paste( power$Date, power$Time ), format="%F %T" )
 #Open the png device
 png(filename = "plot3.png")
 #Plot Global active power (using defaults except for colour and labels)
-with( power, plot( dateTime, Sub_metering_1, type="l", 
+with( power, plot( datetime, Sub_metering_1, type="l", 
                   xlab="", ylab="Energy sub metering" ))
-with( power, points( dateTime, Sub_metering_2, type="l", col="red" ))
-with( power, points( dateTime, Sub_metering_3, type="l", col="blue" ))
+with( power, points( datetime, Sub_metering_2, type="l", col="red" ))
+with( power, points( datetime, Sub_metering_3, type="l", col="blue" ))
 legend( "topright", col=c( "black", "red", "blue" ), lty=1,
     legend = c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ))
 #Close the device to write the plot to disk
